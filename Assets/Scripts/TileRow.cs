05da5d4bb178cd56721983e60ebbb1a5e6a04b4b
lanedorfer <olgalanedorfer@gmail.com>
2024-09-28 03:43:52 +0300

    add main commit, first try

---
+++ Assets/Scripts/TileRow.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class TileRow : MonoBehaviour
+{
+  public TileCell[] cells { get; private set; }
+
+  private void Awake()
+  {
+    cells = GetComponentsInChildren<TileCell>();
+  }
+}
